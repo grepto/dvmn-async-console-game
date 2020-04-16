fc7eea02651630ecb7880689ae1404a7ef006475
grepto <grepto@gmail.com>
2020-04-16 18:32:39 +0300

    increase rocket speed

--- animation/spaceship.py
+++ animation/spaceship.py
@@ -8,12 +8,12 @@ FRAME_DIR = os.path.join('animation', 'frames')
 
 async def spaceship(canvas, start_row, start_column):
     with open(os.path.join(FRAME_DIR, 'rocket_frame_1.txt'), 'r') as rocket:
-        frame1 = rocket.read()
+        next_frame = rocket.read()
     with open(os.path.join(FRAME_DIR, 'rocket_frame_2.txt'), 'r') as rocket:
-        frame2 = rocket.read()
+        active_frame = rocket.read()
 
     canvas_height, canvas_width = canvas.getmaxyx()
-    frame_height, frame_width = get_frame_size(frame1)
+    frame_height, frame_width = get_frame_size(active_frame)
     border_width = 1
 
     row, column = start_row + 1, start_column - round(frame_width / 2)
@@ -22,17 +22,12 @@ async def spaceship(canvas, start_row, start_column):
     motion_space_height = canvas_height - frame_height - border_width
 
     while True:
-        draw_frame(canvas, row, column, frame2, negative=True)
-        rows_direction, columns_direction, space_pressed = read_controls(canvas)
+        draw_frame(canvas, row, column, active_frame, negative=True)
+        row_offset, column_offset, space_pressed = read_controls(canvas)
 
-        if rows_direction:
-            row = min(row + rows_direction, motion_space_height) if rows_direction > 0 else max(row + rows_direction, 1)
+        row = min(row + row_offset, motion_space_height) if row_offset > 0 else max(row + row_offset, 1)
+        column = min(column + column_offset, motion_space_width) if column_offset > 0 else max(column + column_offset, 1)
 
-        if columns_direction:
-            column = min(column + columns_direction, motion_space_width) if columns_direction > 0 else max(column + columns_direction, 1)
-
-        draw_frame(canvas, row, column, frame1)
-        await asyncio.sleep(0)
-        draw_frame(canvas, row, column, frame1, negative=True)
-        draw_frame(canvas, row, column, frame2)
+        draw_frame(canvas, row, column, next_frame)
+        active_frame, next_frame = next_frame, active_frame
         await asyncio.sleep(0)
