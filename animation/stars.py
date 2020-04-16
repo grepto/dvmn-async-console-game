1815ddc49ac78112c717fa54ac087c315079e27f
grepto <grepto@gmail.com>
2020-04-16 15:47:05 +0300

    move randomizing blink outside

--- animation/stars.py
+++ animation/stars.py
@@ -3,11 +3,10 @@ import curses
 import random
 
 
-async def blink(canvas, row, column, symbol='*'):
-    animation_offset = random.randint(5, 20)
+async def blink(canvas, row, column, offset_tics, symbol='*'):
     while True:
 
-        for i in range(animation_offset):
+        for i in range(offset_tics):
             canvas.addstr(row, column, symbol, curses.A_DIM)
             await asyncio.sleep(0)
 
@@ -32,7 +31,8 @@ def get_stars(canvas, symbols='+*.:', quantity=50):
         row = random.randint(1, height - 2)
         column = random.randint(1, width - 2)
         symbol = random.choice(symbols)
-        coroutine = blink(canvas, row, column, symbol)
+        offset_tics = random.randint(5, 20)
+        coroutine = blink(canvas, row, column, offset_tics, symbol)
         coroutines.append(coroutine)
 
     return coroutines
