335767a0ab4f1b5430292bf11f9c937636e76630
grepto <grepto@gmail.com>
2020-04-16 16:47:05 +0300

    change setting to find border touch from rocket

--- animation/spaceship.py
+++ animation/spaceship.py
@@ -14,20 +14,23 @@ async def spaceship(canvas, start_row, start_column):
 
     canvas_height, canvas_width = canvas.getmaxyx()
     frame_height, frame_width = get_frame_size(frame1)
+    border_width = 1
 
     row, column = start_row + 1, start_column - round(frame_width / 2)
 
-    motion_space_width = canvas_width - frame_width
-    motion_space_height = canvas_height - frame_height
+    motion_space_width = canvas_width - frame_width - border_width
+    motion_space_height = canvas_height - frame_height - border_width
 
     while True:
         draw_frame(canvas, row, column, frame2, negative=True)
 
         rows_direction, columns_direction, space_pressed = read_controls(canvas)
-        if rows_direction and row + rows_direction in range(1, motion_space_height):
-            row += rows_direction
-        if columns_direction and column + columns_direction in range(1, motion_space_width):
-            column += columns_direction
+
+        if rows_direction:
+            row = min(row + rows_direction, motion_space_height) if rows_direction > 0 else max(row + rows_direction, 1)
+
+        if columns_direction:
+            column = min(column + columns_direction, motion_space_width) if columns_direction > 0 else max(column + columns_direction, 1)
 
         draw_frame(canvas, row, column, frame1)
         await asyncio.sleep(0)
